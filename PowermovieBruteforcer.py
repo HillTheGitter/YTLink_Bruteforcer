3b9ea0183cdc61d3a8298785d139db58383d6e4f
HillTheGitter <cahill1502@gmail.com>
2021-06-06 21:14:15 +0900

    여러 가지 기능들 추가

--- PowermovieBruteforcer.py
+++ PowermovieBruteforcer.py
@@ -30,18 +30,22 @@ def BruteForce(message):
     print('\n대입을 시작합니다. (경우의 수 ' + str(poss) + '개)')
     
     cal_caseprint(new,'', VideoId)
+    
+    found = '없음'
+    i = 0
 
     for z in VideoId:
         URL = 'https://img.youtube.com/vi/' + z + '/mqdefault.jpg'
         response = requests.get(URL)
+        i = i + 1
+        print("대입 중... (" + str(i) + '/' + str(poss) + ')', end='\r')
         if response.status_code == 200:
             found = '발견됨'
             url = 'https://youtu.be/' + z
-            print('\n\n[영상 링크를 발견했습니다!]\n' + url + '\n\n')
+            print('\n\n[영상 링크를 발견했습니다!]\n' + url + '\n')
             webbrowser.open(url)
     Hi = input('대입이 끝났습니다. (일치하는 결과 ' + found + ')\n또 찍을 코드가 있다면 입력해주세요.\n> ')
     BruteForce(Hi)
 
-found = '없음'
 Hi = input('찍을 코드를 입력해주세요.\n> ')
 BruteForce(Hi)
